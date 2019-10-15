8c3c6aa119d30c8ef5187afa4c04175217b10d98
Selam S. Van Voorhis <selam.vanvoorhis@centre.edu>
2019-10-15 15:28:51 -0400

    Peace

--- java.js
+++ java.js
@@ -1,1 +1,31 @@
-
+var PokemonInfoPromise= d3.json("https://pokeapi.co/api/v2/pokedex")
+PokemonInfoPromise.then(function(pokedex)
+{
+    console.log("pokedex",pokedex)
+    var dex = pokedex.results
+    getRegion(dex)
+    //getmon(pokedex)
+   // getpokemon-species(pokedex)
+                        
+            },
+    function(Error){
+        console.log("fail", Error)
+        
+    })
+//var getmon=function(pokedex)
+            
+var getRegion= function(pokedex)
+{
+    d3.select(".Regions")
+    .selectAll("div")
+    .data(pokedex)
+    .enter()
+    .append("div")
+    .attr("id", "Regions")
+    .text(function(d){
+        return d.name;})
+    .on("click", function(pokedex){
+     getmon(dex)
+    
+    })
+}
